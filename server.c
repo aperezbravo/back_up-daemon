73b98c4309ef667c3cc67d71ea9f78d8ddf04f5a
aperezbravo <aperezbravo@mail.csuchico.edu>
2021-10-05 14:27:51 -0700

    node3: fixed bug opening files for logging

--- server.c
+++ server.c
@@ -9,16 +9,19 @@
 #include<string.h>
 //errno
 #include<errno.h>
-//close(), open(), write()
+//close(), open(), write(), fork(), getpid()
 #include<unistd.h>
 //open()
 #include<fcntl.h>
+//time()
+#include<time.h>
 
 #define BUFF_SIZE 256
+#define PORT "4040"
 
 int main(int argc, char *argv[])
 {
-  if(argc < 2) 
+  /*if(argc < 2) 
   {
     fprintf(stderr, "Usage: <port number>\n");
     return -1;
@@ -32,6 +35,44 @@ int main(int argc, char *argv[])
     fprintf(stderr, "Use undesignated ports 2000-65535\n");
     return -1;
   }
+  */
+
+  //pid_t pid = fork();
+
+  //if( pid == 0)
+  {
+
+  //fprintf("backup_server running pid:");
+  //time_t t;
+  //time(&t);
+  char err[] = "err\0";
+  //void message_buffer[BUFF_SIZE];
+  //char mssg[] = "Hello World\n\0";
+  //char tm[] = "hello the time is %d\n", ctime(&t);
+  //int tm[] = ctime(&t);
+  //strcpy(message_buffer, tm);
+  //FILE *fp = NULL;
+  //int fp = 0;
+  //fp = fopen("/home/pi/daemon_logs/log.txt", "a");
+
+  //int open_new_file = open("/home/pi/daemon_logs/log.txt", O_RDWR | O_CREAT, 0777);
+  //message_buffer = "hello from backup_server"; 
+  int open_new_file = open("/home/pi/daemon_logs/log.txt", O_RDWR | O_CREAT, 0666);
+  if(open_new_file == -1)
+  {
+    fprintf(stderr, "NULL was returned: %s\n", gai_strerror(open_new_file));
+    write(open_new_file, err, 2);
+    return -1;
+  }
+  else
+  {
+     write(open_new_file, err, 2);
+  }
+  /*fprintf(fp, "Running backup_server in the background ... \n");
+  fprintf(fp, "PID: %d\n", getpid());
+  fprintf(fp, "time: %s\n", ctime(&t));
+  fclose(fp);
+  */
 
 
   char requestBuffer[BUFF_SIZE];
@@ -50,9 +91,11 @@ int main(int argc, char *argv[])
 
   //obtain ip address from the server
   int server = 0;
-  if( (server = getaddrinfo(NULL, port_char, &hints, &results)) != 0)
+  if( (server = getaddrinfo(NULL, PORT, &hints, &results)) != 0)
   {
-    fprintf(stderr, "SERVER ERR: getaddrinfo(): errno. %d %s\n", server, gai_strerror(server));
+    //fprintf(stderr, "SERVER ERR: getaddrinfo(): errno. %d %s\n", server, gai_strerror(server));
+    write(open_new_file, err, 2);
+    close(open_new_file);
     return -1;
   }//if
 
@@ -76,14 +119,18 @@ int main(int argc, char *argv[])
 
   if(!cur_address)
   {
-    fprintf(stderr, "SERVER ERR: FAILED TO FIND SPECIFIED \n");
+    //fprintf(stderr, "SERVER ERR: FAILED TO FIND SPECIFIED \n");
+    write(open_new_file, err, 2);
+    close(open_new_file);
     exit(1);
   }
 
   //listen in on specified socket on server
   if( listen(server_socket, 5) == -1 )
   {
-    fprintf(stderr, "SERVER ERR: %s\n", strerror(errno));
+    //fprintf(stderr, "SERVER ERR: %s\n", strerror(errno));
+    write(open_new_file, err, 2);
+    close(open_new_file);
     close(server_socket);
     exit(1);
   }
@@ -96,17 +143,21 @@ int main(int argc, char *argv[])
 
   if(accepted_socket == -1 )
   {
-    fprintf(stderr, "SERVER ERR: %s\n", strerror(errno));
+    //fprintf(stderr, "SERVER ERR: %s\n", strerror(errno));
+    write(open_new_file, err, 2);
+    close(open_new_file);
     close(accepted_socket);
     close(server_socket);
+    exit(1);
   }
 
   close(server_socket);
 
   //finished setup, beign communications
-  int file_path = recv(accepted_socket, (void*)requestBuffer, BUFF_SIZE, 0);
+  //int file_path = recv(accepted_socket, (void*)requestBuffer, BUFF_SIZE, 0);
+  recv(accepted_socket, (void*)requestBuffer, BUFF_SIZE, 0);
 
-  fprintf(stdout, "%s\n", requestBuffer);
+  //fprintf(stdout, "%s\n", requestBuffer);
 
   char *file_name = requestBuffer;
   //memset(requestBuffer, 0, BUFF_SIZE);
@@ -115,16 +166,18 @@ int main(int argc, char *argv[])
   //char tmp[] = "100";
   char tmp[1];
   tmp[0] = 100;
-  fprintf(stdout, "send status %d\n", tmp[0]);
+  //fprintf(stdout, "send status %d\n", tmp[0]);
   send(accepted_socket, tmp, 1, 0); 
 
 
-  fprintf(stdout,"open: %s\n", file_name);
+  //fprintf(stdout,"open: %s\n", file_name);
   int new_file = open(file_name, O_RDWR | O_CREAT, 0777);
   if(new_file == -1)
   {
-    fprintf(stdout, "new file exit\n");
-    fprintf(stdout, "%s\n", gai_strerror(new_file));
+    //fprintf(stdout, "new file exit\n");
+    //fprintf(stdout, "%s\n", gai_strerror(new_file));
+    write(open_new_file, err, 2);
+    close(open_new_file);
     exit(1);
   }
 
@@ -133,22 +186,37 @@ int main(int argc, char *argv[])
 
   while( (read_bytes = recv(accepted_socket, requestBuffer, BUFF_SIZE, 0)) > 0 )
   {
-    fprintf(stdout, "read_bytes: %d\n", read_bytes);
-    fprintf(stdout, "%s", requestBuffer); 
+    //fprintf(stdout, "read_bytes: %d\n", read_bytes);
+    //fprintf(stdout, "%s", requestBuffer); 
     bytes_written = write(new_file, requestBuffer, read_bytes);
     if(bytes_written < 0)
     {
-      fprintf(stderr, "SERVER_ERR: %s\n", gai_strerror(bytes_written));
-      fprintf(stderr, "%d\n", bytes_written);
+      //fprintf(stderr, "SERVER_ERR: %s\n", gai_strerror(bytes_written));
+      //fprintf(stderr, "%d\n", bytes_written);
+      write(open_new_file, err, 2);
+      close(open_new_file);
       exit(1);
     }//
   }//while
-  if(read_bytes < 0)
+  //if(read_bytes < 0)
   {
-    fprintf(stderr, "SERVER ERR: %s\n", gai_strerror(read_bytes));
-    fprintf(stderr, "errno: %d\n", read_bytes);
+    //fprintf(stderr, "SERVER ERR: %s\n", gai_strerror(read_bytes));
+    //fprintf(stderr, "errno: %d\n", read_bytes);
   }//if
   close(accepted_socket);
+  close(open_new_file);
   //close();
+  //return 0;
+
+
+
+
+
+  }//main loop
+  /*else
+  {
+    fprintf(stdout, "backup_server running pid: %d\n", pid);
+    //exit(0);
+  }*/
   return 0;
 }
